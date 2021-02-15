eccf3c46dd1036f79d9559b16075e0af3410d588
Mwrench <mateus.alves.quintela@gmail.com>
2021-02-15 23:13:59 +0000

    Initial commit

---
+++ db.py
@@ -0,0 +1,6 @@
+class Database:
+    def __init__(self):
+        self.Host = 'ec2-52-209-134-160.eu-west-1.compute.amazonaws.com'
+        self.Database = 'dbsfbt6r1ncbnh'
+        self.User = 'casvkwnwlhkaol'
+        self.Password = 'b7e5f828db474bdb83d57d364af2921517e179e8d1b0b8303cf837e32b432457'
